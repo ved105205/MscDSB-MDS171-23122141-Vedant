d42fb2cc71fdf010d294da138c1b0854fb9020dd
ved105205 <vedant.nehal@mscds.christuniversity.in>
2023-10-26 10:57:29 +0530

    update

--- mymodule.py
+++
@@ -1,9 +0,0 @@
-class student:
-    def __init__(self,name,phone):
-        self.name = name
-        self.phone = phone
-
-    def printStudent(self):
-        print(self.name,self.phone)
-
-        
